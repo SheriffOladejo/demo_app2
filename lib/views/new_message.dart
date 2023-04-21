b10c20bff2f1126bfa485b1a61c9974529030c7e
Sheriff Oladejo <sherifffoladejo@gmail.com>
2023-04-21 13:55:24 +0100

    Default commit

--- lib/views/new_message.dart
+++ lib/views/new_message.dart
@@ -15,7 +15,7 @@ class _NewMessageState extends State<NewMessage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("New message", style: TextStyle(
+        title: const Text("New message", style: TextStyle(
             color: Colors.black,
             fontFamily: 'publicsans-regular',
             fontSize: 18),
@@ -24,7 +24,36 @@ class _NewMessageState extends State<NewMessage> {
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-
+        child: Column(
+          children: [
+            Container(height: 10,),
+            Container(
+              margin: const EdgeInsets.all(15),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 1),
+                borderRadius: const BorderRadius.all(Radius.circular(10))
+              ),
+              child: Row(
+                children: [
+                  const Icon(Icons.phone, color: Colors.black,),
+                  Container(width: 5,),
+                  TextField(
+                    decoration: InputDecoration(
+                      prefix: const Icon(Icons.phone, color: Colors.black,),
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(16),
+                      )
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
